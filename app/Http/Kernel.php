2b242a9a7046a5246b2cd4490083fc7fa1f14dab
Bob-geniuS <alvarez.daniel.9376@gmail.com>
2025-07-10 13:45:23 -0400

    Registro del middleware 'verify.apikey' => \App\Http\Middleware\VerifyApiKey::class' en app/Http/Kernel.php protected

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        
+        // para API KEY
+        'verify.apikey' => \App\Http\Middleware\VerifyApiKey::class,
     ];
 }
